4ae4c6f9775d6f173e776e24489ccce6100a16af
sandeep18p <sandeep.pansari98@gmail.com>
2024-10-14 01:58:37 +0530

    added_animation

--- script.js
+++ script.js
@@ -17,23 +17,23 @@ hamburger.addEventListener('click', () => {
 });
 
 
-    // function randomizeBirdFlight() {
-    //     const bird = document.getElementById('bird');
-    //     const screenHeight = window.innerHeight;
+    function randomizeBirdFlight() {
+        const bird = document.getElementById('bird');
+        const screenHeight = window.innerHeight;
 
-    //     // Randomize flight duration
-    //     const randomDuration = Math.random() * (15 - 8) + 8; // Random duration between 8 to 15 seconds
-    //     bird.style.animationDuration = randomDuration + 's';
+        // Randomize flight duration
+        const randomDuration = Math.random() * (15 - 8) + 8; // Random duration between 8 to 15 seconds
+        bird.style.animationDuration = randomDuration + 's';
 
-    //     // Randomize top starting position
-    //     const randomStartTop = Math.random() * (screenHeight * 0.3); // Random starting top position within 30% of screen height
-    //     bird.style.top = randomStartTop + 'px';
+        // Randomize top starting position
+        const randomStartTop = Math.random() * (screenHeight * 0.3); // Random starting top position within 30% of screen height
+        bird.style.top = randomStartTop + 'px';
 
-    //     // Repeat the function after each animation
-    //     setTimeout(randomizeBirdFlight, randomDuration * 1000); // Run again after current animation completes
-    // }
+        // Repeat the function after each animation
+        setTimeout(randomizeBirdFlight, randomDuration * 1000); // Run again after current animation completes
+    }
 
-    // randomizeBirdFlight(); // Initial call to start the animation
+    randomizeBirdFlight(); // Initial call to start the animation
 
 
 document.addEventListener('scroll', function() {
